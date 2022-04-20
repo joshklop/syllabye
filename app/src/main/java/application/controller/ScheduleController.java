194aa07221cff3b8e4af3fed85a77045eb9fe74e
Josh Klopfenstein <joshklop10@gmail.com>
2022-04-20 17:19:48 -0500

    Dynamically add labels to gridpane

--- app/src/main/java/application/controller/ScheduleController.java
+++ app/src/main/java/application/controller/ScheduleController.java
@@ -3,7 +3,6 @@ package application.controller;
 import application.model.Database;
 import application.model.Syllabus;
 import javafx.fxml.FXML;
-import javafx.scene.layout.GridPane;
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,6 +17,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.time.DayOfWeek;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class ScheduleController implements Initializable {
     @FXML
@@ -39,12 +41,50 @@ public class ScheduleController implements Initializable {
         }
         // TODO sort semesters by month, year
         semesterComboBox.getItems().addAll(syllabyeBySemester.keySet());
+        semesterComboBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == null || newValue == oldValue) {
+                return;
+            }
+            // Remove existing labels
+            // Add new labels
+            for (Syllabus s : syllabyeBySemester.get(newValue)) {
+                for (DayOfWeek d : s.getLectureDayTimes().keySet()) {
+                    switch (d) {
+                        case MONDAY:
+                            agenda.add(makeLabel(s, d), 0, nextEmptyCell(0));
+                            break;
+                        case TUESDAY:
+                            agenda.add(makeLabel(s, d), 1, nextEmptyCell(1));
+                            break;
+                        case WEDNESDAY:
+                            agenda.add(makeLabel(s, d), 2, nextEmptyCell(2));
+                            break;
+                        case THURSDAY:
+                            agenda.add(makeLabel(s, d), 3, nextEmptyCell(3));
+                            break;
+                        case FRIDAY:
+                            agenda.add(makeLabel(s, d), 4, nextEmptyCell(4));
+                            break;
+                    }
+                }
+            }
+        });
     }
 
-    public void selectSemester(ActionEvent e) {
-        syllabyeBySemester.get(((ComboBox<String>)e.getSource()).getValue());
-        // TODO Remove all labels
-        // Add labels
+    private int nextEmptyCell(int column) {
+        for (int i = column; i < agenda.getColumnCount() * agenda.getRowCount(); i += agenda.getColumnCount()) {
+            System.out.println("hello");
+            System.out.println(agenda.getChildren().get(i));
+            if (agenda.getChildren().get(i).equals(null)) {
+                return i / agenda.getColumnCount();
+            }
+        }
+        // This should overwrite the last item in the column
+        return column * agenda.getRowCount();
+    }
+
+    private Label makeLabel(Syllabus s, DayOfWeek d) {
+        return new Label(s.getCourseSubject() + " " + s.getCourseNumber() + "\n" + s.getLectureDayTimes().get(d));
     }
 
     public void goToSelectionPage(ActionEvent e) throws IOException {
